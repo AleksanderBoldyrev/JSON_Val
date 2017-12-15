92d893dce0196256570a7a8385d1a57c0ad2d595
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2017-12-16 01:23:35 +0300

    Update Main.java

--- src/main/java/com/alex/Main.java
+++ src/main/java/com/alex/Main.java
@@ -17,16 +17,3 @@ public class Main {
         System.out.println("Server stopped");
     }
 }
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Creating server");
-        Netw_Layer servDaemon = new Netw_Layer(Settings.PORT);
-        System.out.println("Creating server thread");
-        Thread serverThread = new Thread(servDaemon);
-        System.out.println("Trying to run server thread");
-        serverThread.run();
-        System.out.println("Server stopped");
-    }
-}
