c7bd40873db5402cbfb1016a5e86276e44efd625
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2017-12-16 01:26:50 +0300

    Update Core.java

--- src/main/java/com/alex/Core.java
+++ src/main/java/com/alex/Core.java
@@ -1,9 +1,6 @@
 package com.alex;
 
 import java.io.IOException;
-//import org.json.simple.JSONArray;
-//import org.json.simple.JSONObject;
-//import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
@@ -26,23 +23,24 @@ public class Core {
 
     Core() {
     }
-
+     /*
+     * Processing core function
+     * <p>
+     * The method returns full JSON file, or an error message instead in case there occured validation errors.
+     *
+     * @throws ParseEception in case there were validating problems.
+     */
     public String validate(String in) {
         JSONParser parser = new JSONParser();
         try {
             JSONObject object = (JSONObject) parser.parse(in);
             Set keys = object.keySet();
-            //for (int i=0; i< keys.size(); i++)
-                //System.out.println("KEY: " + keys.toArray()[i].toString());
         } catch (ParseException ex) {
             JSONObject error = new JSONObject();
 
             error.put(Settings.ERR_CODE,ex.getErrorType());
             error.put(Settings.ERR_MES, ex.toString());
             error.put(Settings.ERR_PL,ex.getPosition());
-            //System.out.print(error.toString());
-
-            //System.out.println("Json error: " + ex.toString());
 
             return error.toString();
         }
