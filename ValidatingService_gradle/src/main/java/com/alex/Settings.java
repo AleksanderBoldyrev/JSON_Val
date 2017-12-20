1d99050e6e9e623ce9210bf4b3eb173a969d80d4
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2017-12-20 19:49:49 +0300

    jar removed

---
+++ ValidatingService_gradle/src/main/java/com/alex/Settings.java
@@ -0,0 +1,27 @@
+package com.alex;
+//import com.sun.org.apache.xerces.internal.xs.StringList;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 06.11.2017.
+ */
+public final class Settings {
+    public static final int PORT = 80;
+    public static final String HOST = "localhost";
+    public static final int SLEEP_TIME = 50;
+    public static final int RETRIES_COUNT = 200;
+    public static final String REQ_CONT_LEN = "Content-Length: ";
+
+    public static final String ERR_CODE = "errorCode";
+    public static final String ERR_MES = "errorMessage";
+    public static final String ERR_PL = "errorPlace";
+
+    public static final ArrayList<Character> NOT_VALID_CHARS = new ArrayList<Character>() {{
+        //add('\r');
+        //add('\n');
+        add('\u0000');
+        add('\uFEFF');
+        //add(' ');
+    }};
+}
---
+++ run.sh
@@ -0,0 +1,4 @@
+wget -P ./jar/ http://central.maven.org/maven2/com/googlecode/json-simple/json-simple/1.1/json-simple-1.1.jar
+mkdir bin
+javac -cp ".:./jar/json-simple-1.1.jar" -d ./bin ./src/main/java/com/alex/*.java
+sudo java -classpath ./bin:./jar/json-simple-1.1.jar com.alex.Main
